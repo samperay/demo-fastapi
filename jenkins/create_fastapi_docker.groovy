d429302f21eae0d2569e5f292ce3039177170315
samperay <sunlnx@gmail.com>
2023-01-05 15:29:24 +0530

    fastapi jenkins docker groovy script

---
+++ jenkins/create_fastapi_docker.groovy
@@ -0,0 +1,21 @@
+
+
+job('demo-fastapi') {
+    scm {
+        git('https://github.com/samperay/demo-fastapi.git') {
+            displayName("demo-fastapi")
+            description("Project for demo jenkins-dsl and fastapi")
+        }
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('sunlnx/demo-fastapi')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
